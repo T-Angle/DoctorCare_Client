7dd8efa1c9506a20e9a956b872cbf75a9b79272f
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-11-28 23:10:57 +0700

    add,update library , pages login , register

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
