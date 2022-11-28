69d6b5bc98e65923261ca208f1a5f07a9c35e564
MinhGH49 <minhtq.49@gmail.com>
2022-11-29 00:34:08 +0700

    sua client moi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/index";
+import HomeIndex from "./pages/Home/index";
+import AboutIndex from "./pages/About/index";
+import ContentIndex from "./pages/Contact/index";
+import SearchResultIndex from "./pages/SearchResult/index";
+// //Auth
+import LoginIndex from "./pages/Auth/Login/index";
+import RegisterIndex from "./pages/Auth/Register/index";
+import ResetIndex from "./pages/Auth/Reset/index";
+import AdminLogin from "./pages/Auth/Admin/Login";
+
+// //Account
+import DoctorAccountMaster from "./pages/Account/Doctor/Master/index";
+import PatientAccountMaster from "./pages/Account/Patient/Master/index";
+import AdminMaster from "./pages/Account/Admin/Master/index";
+import PrivateRoute from "./components/PrivateRoute/index";
+
+// //Chat
+
+import ChatIndex from "./pages/Chat/index";
+
+import FourOFour from "./pages/FourOFour/index";
+
+// appointments
+import Test from "./Appointment/test"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={HomeIndex} />
+            <Route exact path="/about-us" component={AboutIndex} />
+            <Route exact path="/contact-us" component={ContentIndex} />
+            <Route exact path="/search" component={SearchResultIndex} />
+            <Route exact path="/login" component={LoginIndex} />
+            <Route exact path="/register" component={RegisterIndex} />
+            <Route exact path="/reset" component={ResetIndex} />
+            <Route exact path="/admin-login" component={AdminLogin} />
+
+
+            <Route exact path="/appointment-app" component={Test} />
+
+            {/* Doctor Master */}
+            <PrivateRoute path="/doctor" role="doctor">
+              <DoctorAccountMaster />
+            </PrivateRoute>
+
+            {/* Patient Master */}
+            <PrivateRoute path="/patient" role="patient">
+              <PatientAccountMaster />
+            </PrivateRoute>
+
+            {/* Council Master */}
+            {/* <Route exact path="/messages/:reciverId/:appointmentId"> */}
+            <Route exact path="/messages">
+              <ChatIndex />
+            </Route>
+
+            {/* Admin Master */}
+            <PrivateRoute exact path="/admin" role="super_admin">
+              <AdminMaster />
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/admin" role="admin">
+              <AdminMaster />
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/admin" role="manager">
+              <AdminMaster />
+            </PrivateRoute>
+            <Route path="/admin" role="admin">
+             <AdminMaster />
+           </Route>
+
+            <Route path="*" component={FourOFour} />
+          </Switch>
+        </ScrollToTop>
+      </Router>
     </div>
   );
 }
---
+++ src/Appointment/test.js
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <>
+      <h1>sand box here</h1>
+    </>
+  )
+}
