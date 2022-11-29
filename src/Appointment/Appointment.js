5450197a1b5794201547f226274a2656ed68663a
MinhGH49 <minhtq.49@gmail.com>
2022-11-30 02:34:24 +0700

    them layout chuc nang kham benh

--- src/Appointment/Appointment.js
+++ src/Appointment/Appointment.js
@@ -1,31 +1,49 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+//import router
+import { Route, Switch, Link, BrowserRouter } from 'react-router-dom'
 
-const appRoot = '/appointment-app'
+//import component
+import CounterCard from './components/counterCard/CounterCard'
+import QrCard from './components/qrCard/QrCard'
+// import Chat from './pages/testChatApp/Chat'
+import Examination from './pages/medExamination/Examination'
+import Navigation from './components/navigation/Navigation'
+import { Toolbar } from '@mui/material'
 
+const baseURL = "/appointment-app"
+
+function DevTestsPage() {
+  return (
+    <>
+      <QrCard/>
+      <CounterCard order='15' currentOrder='2' orderPickedTime='14:30'/>
+
+    </>
+  )
+}
+
+function DevTreatmentPage() {
+  return (
+    <>
+      <h1>Treatment page</h1>
+    </>
+  )
+}
 //day la entry point cua app thuc hien kham benh
 //folder Appointment se dc to chuc nhu 1 react app, trong do App.js == Appointment.js (file nay)
 
 export default function Appointment() {
   return (
-    <>
-      <h1>sand box here</h1>
-      <BrowserRouter>
-        <nav>
-          <ul>
-            <li><Link to={appRoot+'/a'}>A</Link></li>
-            <li><Link to={appRoot+'/b'}>B</Link></li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path={appRoot+'/a'}>
-            <h1>this is a</h1>
-          </Route>
-          <Route path={appRoot+'/b'}>
-            <h1>this is b</h1>
-          </Route>
-        </Switch>
+      <div className="Appointment">
+        <Toolbar/>
+        <BrowserRouter>
+          <Navigation/>
+          <Switch>
+            <Route exact path={baseURL + "/"} component={Examination}/>
+            <Route exact path={baseURL + "/tests"} render={props => <DevTestsPage {...props}/>} />
+            <Route exact path={baseURL + "/treatment"} render={props => <DevTreatmentPage {...props}/>} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
 
-      </BrowserRouter>
-    </>
-  )
 }
